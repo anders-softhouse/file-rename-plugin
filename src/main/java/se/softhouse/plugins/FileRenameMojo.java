0a2c3dd035fb001efa199ae052d1ec6b246f74f9
Anders Björklund <anders.bjorklund@teliacompany.com>
2023-11-07 13:48:53 +0100

    Now reading source and target dirrectories. Making them absolute if relative.

--- src/main/java/se/softhouse/plugins/FileRenameMojo.java
+++ src/main/java/se/softhouse/plugins/FileRenameMojo.java
@@ -8,7 +8,10 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
+import java.io.File;
+
 import java.util.List;
+import java.util.ArrayList;
 
 @Mojo(name = "rename", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class FileRenameMojo extends AbstractMojo {
@@ -16,24 +19,59 @@ public class FileRenameMojo extends AbstractMojo {
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
     MavenProject project;
 
-    @Parameter( name = "sourceDir", property = "sourceDir", required = false)
+    @Parameter(name = "sourceDir", property = "sourceDir", required = false)
     String sourceDir;
 
-    @Parameter( name = "targetDir", property = "targetDir", required = false)
+    @Parameter(name = "targetDir", property = "targetDir", required = false)
     String targetDir;
 
-    @Parameter( name = "rules", property = "rules")
-    private List< Rule > rules;
-
+    @Parameter(name = "rules", property = "rules")
+    private List<Rule> rules;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("SOURCE: " + sourceDir);
-        getLog().info("TARGET: " + targetDir);
 
-        for( Rule rule : rules ) {
-            getLog().info("RULE: " + rule.getFileRegex());
+        // Make sure directory paths are absolute. Check if relative and if so add base
+        // directory.
+        File source = new File(sourceDir);
+
+        if (!source.isAbsolute()) {
+            source = new File(project.getBasedir() + File.separator + sourceDir);
         }
+
+        File target = new File(targetDir);
+
+        if (!target.isAbsolute()) {
+            target = new File(project.getBasedir() + File.separator + targetDir);
+        }
+
+        getLog().info("SOURCE: " + source.getAbsolutePath());
+        getLog().info("TARGET: " + target.getAbsolutePath());
+
+        File[] files = source.listFiles();
+        List<File> filesCurrated = new ArrayList<>();
+
+        for (File file : filesCurrated) {
+
+            if (file.isFile()) {
+                filesCurrated.add(file);
+            }
+
+        }
+
+        for (File file : filesCurrated) {
+
+            for (Rule rule : rules) {
+
+                if(  ) {
+
+                }
+
+                getLog().info("RULE: " + rule.getFileRegex());
+            }
+
+        }
+
     }
 
 }
